e3c3ca9e63319a1034c87eb3220fa9a45d86ec29
SirMauricio <190300383@unicaribe.edu.mx>
2025-06-06 17:54:29 -0500

    Carrusel tours

---
+++ src/data/toursCards.ts
@@ -0,0 +1,35 @@
+const restaurantsCards = [
+  {
+    title: 'Turibus Real del Monte',
+    place: 'REAL DEL MONTE',
+    image: new URL('@/assets/img/tours/ElSerranillo.jpg', import.meta.url).href,
+  },
+  {
+    title: 'Minas de Real del monte',
+    place: 'REAL DEL MONTE',
+    image: new URL('@/assets/img/tours/TourMinas.jpg', import.meta.url).href,
+  },
+  {
+    title: 'Museo del Paste',
+    place: 'REAL DEL MONTE',
+    image: new URL('@/assets/img/tours/MuseoDelPaste.jpg', import.meta.url).href,
+  },
+  {
+    title: 'Excursión a los Prismas Basalticos',
+    place: 'REAL DEL MONTE',
+    image: new URL('@/assets/img/tours/PrismasBasalticos.jpg', import.meta.url).href,
+  },
+  {
+    title: 'Tour en Bicicleta de Montaña por Mineral del Monte',
+    place: 'REAL DEL MONTE',
+    image: new URL('@/assets/img/restaurants/TourEnBicicleta.jpg', import.meta.url).href,
+  },
+  {
+    title: 'Visita al Centro Histórico Real del Monte',
+    place: 'REAL DEL MONTE',
+    image: new URL('@/assets/img/restaurants/CentroHistorico.jpg', import.meta.url).href,
+  }
+  
+]
+
+export default restaurantsCards
